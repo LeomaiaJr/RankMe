2414cea7f69e71839444d688582ae26b913d6ae3
Teles <lucas@pdsolucoes.com.br>
2022-09-06 21:44:10 -0300

    feat: adds question edit page

--- src/app/modules/auth/redux/QuestionCRUD.tsx
+++
@@ -1,9 +0,0 @@
-import { api } from "../../../../infra/api";
-
-export function createQuestion(question: any) {
-  return api.post("/questions", question);
-}
-
-export function deleteQuestion(id: string) {
-  return api.delete(`/questions/${id}`);
-}
